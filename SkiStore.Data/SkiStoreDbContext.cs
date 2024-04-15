9a78adf596b15a96324ffe4dbcd96715c56daac0
EmadERagheb <emaderagheb@gmail.com>
2024-04-15 17:48:51 +0200

    Product Table Created

--- SkiStore.Data/SkiStoreDbContext.cs
+++ SkiStore.Data/SkiStoreDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SkiStore.Domain.Models;
 
 namespace SkiStore.Data
 {
     public class SkiStoreDbContext:DbContext
     {
+        public DbSet<Product> Products { get; set; }
         public SkiStoreDbContext(DbContextOptions<SkiStoreDbContext> options):base(options)
         {
             
