5d4916df8a49f0929bfc0e3b02fecf36aaf7977d
EmadERagheb <emaderagheb@gmail.com>
2024-04-22 16:04:05 +0200

    configure lazy loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopComponent } from './shop/shop.component';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 
 
@@ -21,7 +18,6 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule
   ],
   providers: [],
