9ff266efa2b6ac9bdc21efce5c0f0c5191d843ff
EmadERagheb <emaderagheb@gmail.com>
2024-04-23 19:07:18 +0200

    create error components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { ErrorTestComponent } from './error-test/error-test.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    ErrorTestComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     CommonModule,
