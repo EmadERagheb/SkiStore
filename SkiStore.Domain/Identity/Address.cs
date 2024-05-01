9c4e424ce7d5e5186d912e52a044e7ae9323a59d
EmadERagheb <emaderagheb@gmail.com>
2024-05-01 20:47:11 +0300

    install required packages and implement user class

---
+++ SkiStore.Domain/Identity/Address.cs
@@ -0,0 +1,33 @@
+﻿// Ignore Spelling: App
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SkiStore.Domain.Identity
+{
+    public class Address
+    {
+        #region Columns
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        [Required]
+        public string AppUserId { get; set; }
+        #endregion
+        #region Rs
+        #region Address-User RS
+        //user Has one Address
+        //column AppUserId
+        //required
+        //navigation probertiy
+        public AppUser AppUser { get; set; }
+        #endregion
+        #endregion
+
+    }
+}
---
+++ SkiStore.Domain/Identity/AppUser.cs
@@ -0,0 +1,25 @@
+﻿// Ignore Spelling: App
+
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkiStore.Domain.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        #region Column
+        public string DisplayName { get; set; }
+        [Required]
+        public int AddressId { get; set; }
+        #endregion
+        #region RS
+        #region User-Address RS
+        //user must has one address
+        //column AddressID
+        //required
+        //navigation property
+        public Address Address { get; set; }
+        #endregion
+        #endregion
+    }
+}
