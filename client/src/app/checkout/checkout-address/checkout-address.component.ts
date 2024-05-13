2eaf2901651ee8d3d1bb1355d828a993d5307122
EmadERagheb <emaderagheb@gmail.com>
2024-05-13 17:27:52 +0300

    add checkout-address form

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-address',
   templateUrl: './checkout-address.component.html',
-  styleUrls: ['./checkout-address.component.scss']
+  styleUrls: ['./checkout-address.component.scss'],
 })
 export class CheckoutAddressComponent {
-
+  // This is the form group that contains all of our individual controls
+  //for each field in the checkout process (e.g., first name, last name, street address, etc
+  @Input() checkoutForm?: FormGroup;
 }
