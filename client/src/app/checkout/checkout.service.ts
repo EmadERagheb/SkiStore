223673fcc87aab093b75a0c86fae86b20da6f6ce
EmadERagheb <emaderagheb@gmail.com>
2024-05-13 18:38:57 +0300

    add delivery method form and add jwt interceptor

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
-
-  constructor() { }
+  baseURL = environment.apiUrl;
+  constructor(private httpClient: HttpClient) {}
+  getDeliveryMethods() {
+    return this.httpClient
+      .get<DeliveryMethod[]>(`${this.baseURL}Orders/deliveryMethods`)
+      .pipe(map((dm) => dm.sort((a, b) => b.price - a.price)));
+  }
 }
