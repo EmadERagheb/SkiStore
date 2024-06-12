9bb3aa6e1e922a4b00d6a04d480eb35c725e4a64
EmadERagheb <emaderagheb@gmail.com>
2024-06-12 13:24:14 +0300

    download strip package and create payment service

---
+++ SkiStore.Data/Repositories/PaymentService.cs
@@ -0,0 +1,18 @@
+﻿using SkiStore.Domain.Contracts;
+using SkiStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiStore.Data.Repositories
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<CustomerBasket> CreateOrUpdatePayment(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SkiStore.Domain/Contracts/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using SkiStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiStore.Domain.Contracts
+{
+    public interface IPaymentService
+    {
+        Task<CustomerBasket> CreateOrUpdatePayment(string basketId);
+    }
+}
