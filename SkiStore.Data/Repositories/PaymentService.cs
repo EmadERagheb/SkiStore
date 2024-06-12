73d3212b6f4c2efc61c735cf8bede7c90f1df96a
EmadERagheb <emaderagheb@gmail.com>
2024-06-12 14:17:19 +0300

    implement payment services method

--- SkiStore.Data/Repositories/PaymentService.cs
+++ SkiStore.Data/Repositories/PaymentService.cs
@@ -1,18 +1,70 @@
-﻿using SkiStore.Domain.Contracts;
+﻿using Microsoft.Extensions.Configuration;
+using SkiStore.Domain.Contracts;
+using SkiStore.Domain.DTOs.Order;
+using SkiStore.Domain.DTOs.Product;
 using SkiStore.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SkiStore.Domain.Models.OrderAggregate;
+using Stripe;
+using Product = SkiStore.Domain.Models.Product;
 
 namespace SkiStore.Data.Repositories
 {
     public class PaymentService : IPaymentService
     {
-        public Task<CustomerBasket> CreateOrUpdatePayment(string basketId)
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IBasketRepository _basketRepository;
+
+        public PaymentService(IConfiguration configuration, IUnitOfWork unitOfWork, IBasketRepository basketRepository)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+            _basketRepository = basketRepository;
+        }
+        public async Task<CustomerBasket> CreateOrUpdatePayment(string basketId)
+        {
+            StripeConfiguration.ApiKey = _configuration["StripSettings:SecretKey"];
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+            var shippingPrice = 0m;
+            if (basket.DeliveryMethodId is not null)
+            {
+                var delevieryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetAsync<DeliveryMethodDTO>(d => d.Id == basket.DeliveryMethodId);
+                shippingPrice = delevieryMethod.Price;
+            }
+            foreach (var item in basket.Items)
+            {
+                var product = await _unitOfWork.Repository<Product>().GetAsync<GetProductDTO>(p => p.Id == item.Id);
+                item.Price = product.Price;
+
+            }
+            //Strip 
+            var serviecs = new PaymentIntentService();
+            //Create intent
+            if (string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var options = new PaymentIntentCreateOptions()
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string>() { "card" }
+                    
+                };
+               var intent= await serviecs.CreateAsync(options);
+                basket.PaymentIntentId= intent.Id;
+
+            }
+            else
+            {
+                var options = new PaymentIntentUpdateOptions()
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100,
+
+                };
+                serviecs.Update(basket.PaymentIntentId, options);
+            }
+          await  _basketRepository.UpdateBasketAsync(basket);
+            return basket;
+
         }
     }
 }
